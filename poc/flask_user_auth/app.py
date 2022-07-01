a33fcee637747ff257559a83cc6811d3e2b62cf3
Yannick Bouillard <yannick.bouillard@inria.fr>
2022-07-01 18:13:43 +0200

    modifying cookies params and enabling https

--- poc/flask_user_auth/app.py
+++ poc/flask_user_auth/app.py
@@ -1,10 +1,12 @@
+from http import HTTPStatus
 import os
 from site import USER_SITE
-from flask import Flask, jsonify, request
+from typing import Optional
+from flask import Flask, jsonify, request, redirect, Response, url_for
 from database import Database
 import flask_security
 from flask_security import  auth_required, UserMixin
-from flask_login import LoginManager, login_required, login_user#, UserMixin
+from flask_login import LoginManager, login_required, login_user, logout_user#, UserMixin
 from flask_login import login_user
 
 app = Flask(__name__)
@@ -14,7 +16,18 @@ node_database = Database('node_db.json')
 gui_database = Database('gui_db.json')
 #print("DEUG", app.config['NODE_DB_PATH'], app.config['GUI_DB_PATH'])
 
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY", 'pf9Wkove4IKEAXvy-cQkeDPhv9Cb3Ag-wyJILbq_dFw')
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY",
+                                          'pf9Wkove4IKEAXvy-cQkeDPhv9Cb3Ag-wyJILbq_dFw')  # to be generated when running environment scripts
+
+app.config['REMEMBER_COOKIE_HTTPONLY'] = False
+app.config['REMEMBER_COOKIE_SECURE'] = True
+
+app.config['SESSION_COOKIE_SECURE'] = True
+app.config['SESSION_COOKIE_HTTPONLY'] = False
+
+ENABLE_REMEMBER_ME = True
+
+login_manager.session_protection = "strong"  # add extra security for REMEMBER_ME cookies (prevent cookies thieves)
 
 class User(UserMixin):
     fs_uniquifier = 12345
@@ -37,8 +50,23 @@ def create_user():
     query = gui_database.query()
     
     table.insert({"login": "user",
-                  "password":1234})
+                  "password": 1234})
 
+@app.before_request
+def before_request():
+    # redirect connections from HTTP to HTTPS
+    if not request.is_secure:
+        url = request.url.replace('http://', 'https://', 1)
+        code = 301
+        return redirect(url, code=code)
+
+# def https_redirect() -> Optional[Response]:
+#     if request.scheme == 'http':
+#         return redirect(url_for(request.endpoint,
+#                                 _scheme='https',
+#                                 _external=True),
+#                         HTTPStatus.PERMANENT_REDIRECT)
+        
 @app.route("/")
 def unprotected():
     return "<p>this content is not protected</p>"
@@ -49,13 +77,16 @@ def unprotected():
 def protected():
     return "<p>this content is protected</p>"
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['POST', 'GET'])
 def login():
     req = request.json
+    
+    if req is None:
+        return "Please login"
     login = req.get('login', None)
     passwd = req.get('password', None)
     
-    if any((login, passwd)) is None:
+    if not all((login, passwd)):
         print("ERROR: login or passwd missing")
     
     table = gui_database.db().table('_default')
@@ -67,11 +98,27 @@ def login():
     
     if res is not None:
         print("user authenticated!")
-        login_user(USER)
+        login_user(USER, remember=ENABLE_REMEMBER_ME)
     else: 
         print("user auth failed!")
+        # TODO: do the redirection
     return jsonify({'success': True}), 200
+
+@app.route("/logout", methods=['GET'])
+def logout2():
+    logout_user()
+    print("disconnection successful")
+    return redirect("/")
+
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return USER.get(user_id)
+
+# app.before_request(https_redirect)
+
+
+if __name__ == "__main__":
+    
+    app.run(ssl_context='adhoc')

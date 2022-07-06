29af6274629d3109c096715ec2c79423ced0d76a
Yannick Bouillard <yannick.bouillard@inria.fr>
2022-07-06 14:58:04 +0200

    adding user account management

--- poc/flask_user_auth/app.py
+++ poc/flask_user_auth/app.py
@@ -2,12 +2,17 @@ from http import HTTPStatus
 import os
 from site import USER_SITE
 from typing import Optional
-from flask import Flask, jsonify, request, redirect, Response, url_for
+from functools import wraps
+
+#from argon2 import PasswordHasher
+from flask import Flask, jsonify, request, redirect, Response, abort, url_for
 from database import Database
 import flask_security
 from flask_security import  auth_required, UserMixin
 from flask_login import LoginManager, login_required, login_user, logout_user#, UserMixin
-from flask_login import login_user
+from flask_login import login_user, current_user
+
+from flask_perm import Perm
 
 app = Flask(__name__)
 login_manager = LoginManager()
@@ -19,6 +24,7 @@ gui_database = Database('gui_db.json')
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY",
                                           'pf9Wkove4IKEAXvy-cQkeDPhv9Cb3Ag-wyJILbq_dFw')  # to be generated when running environment scripts
 
+# setting for authentification cookies 
 app.config['REMEMBER_COOKIE_HTTPONLY'] = False
 app.config['REMEMBER_COOKIE_SECURE'] = True
 
@@ -29,16 +35,42 @@ ENABLE_REMEMBER_ME = True
 
 login_manager.session_protection = "strong"  # add extra security for REMEMBER_ME cookies (prevent cookies thieves)
 
+
+
+
 class User(UserMixin):
     fs_uniquifier = 12345
+    roles = ["BasicUser"]
     # def is_authenticated(self):
     #     return False
+    
+    def __init__(self):
+        self.username = None
+        self.password = None
+
     def is_active(self):
         return True
     def get(self,  id=None):
         return self
+
+def admin_login(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        
+        print("USER", USER.roles)
+        if not check_if_user_admin(USER):
+            abort(401)
+        else:
+            value = func(*args, **kwargs)
+        return value
+    return wrapper
+    
     # def is_anonymous(self):
     #     return False
+
+
+class UserRoles():
+    pass
 
 USER = User()
 
@@ -49,15 +81,17 @@ def create_user():
     
     query = gui_database.query()
     
-    table.insert({"login": "user",
-                  "password": 1234})
+    table.update({"login": "user",
+                  "password": 12345, 
+                  "role": 'admin'})
 
 @app.before_request
 def before_request():
     # redirect connections from HTTP to HTTPS
     if not request.is_secure:
+        print("REDIRECTION")
         url = request.url.replace('http://', 'https://', 1)
-        code = 301
+        code = 301 #http permanent redirect
         return redirect(url, code=code)
 
 # def https_redirect() -> Optional[Response]:
@@ -95,29 +129,62 @@ def login():
     query = gui_database.query()
     
     res = table.get((query.login == login) & (query.password == passwd))
+    print(res)
+    is_admin = True if res.get("role") == 'admin' else False
+    print("is_admin", is_admin)
     
     if res is not None:
         print("user authenticated!")
+        USER.username = login
+        USER.password = passwd
         login_user(USER, remember=ENABLE_REMEMBER_ME)
+        
+        if is_admin:
+            USER.roles = ['admin']  # TODO: use enum classes instead
+        return jsonify({'success': True}), 200
     else: 
         print("user auth failed!")
         # TODO: do the redirection
-    return jsonify({'success': True}), 200
+        return jsonify({'success': False}), 401
+    
 
 @app.route("/logout", methods=['GET'])
 def logout2():
     logout_user()
+    USER = User()
     print("disconnection successful")
+    # TODO : redirect towards login webpage
     return redirect("/")
 
-
+# protect a view with a principal for that need
+@app.route('/admin')
+@login_required
+@admin_login
+def do_admin_index():
+    return Response('Only if you are an admin')
 
 @login_manager.user_loader
 def load_user(user_id):
-    return USER.get(user_id)
+    user = User()
+    return USER#.get(user_id)
 
 # app.before_request(https_redirect)
 
+def check_if_user_admin(user: User) -> bool:
+    login = user.username
+    passwd = user.password
+    table = gui_database.db().table('_default')
+    table.clear_cache()
+    
+    query = gui_database.query()
+    res = table.get((query.login == login) & (query.password == passwd))
+    print("RES", res)
+    if res is not None and res.get("role") == "admin":
+        user.roles = ["admin"]
+        return True
+    else:
+        user.roles = ["BasicUser"]
+        return False 
 
 if __name__ == "__main__":
     
